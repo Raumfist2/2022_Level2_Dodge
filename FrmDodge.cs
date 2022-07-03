c43491886ebd5ccca9c4277f93da5e1345f8de0f
Leo cook <leocook@mmc.school.nz>
2022-07-04 11:00:06 +1200

    Started working for once

---
+++ FrmDodge.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2022_Level2_Dodge
+{
+    public partial class FrmDodge : Form
+    {
+        Graphics g; //declare a graphics object called g
+        // declare space for an array of 7 objects called planet 
+        Planet[] planet = new Planet[7];
+
+        public FrmDodge()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 7; i++)
+            {
+                int x = 10 + (i * 55);
+                planet[i] = new Planet(x);
+            }
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            for (int i = 0; i < 7; i++)
+            {
+                //call the Planet class's drawPlanet method to draw the images
+                planet[i].DrawPlanet(g);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                planet[i].MovePlanet();
+                //if a planet reaches the bottom of the Game Area reposition it at the top
+                if (planet[i].y >= PnlGame.Height)
+                {
+                    planet[i].y = 30;
+                }
+
+            }
+            PnlGame.Invalidate();//makes the paint event fire to redraw the panel
+        }
+    }
+}
